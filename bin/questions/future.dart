4bd4ed5705a2f6cbf511032879af05174c0c3962
dudurb17 <eduardo.robettibedin@gmail.com>
2024-05-09 09:02:53 -0300

    fututo e async e await

---
+++ bin/questions/future.dart
@@ -0,0 +1,16 @@
+void main() {
+  Future myFuture = Future(() {
+    print("Going back to the Future");
+    return 21;
+  }).then((value) => print("The value is $value"));
+
+  Future<int> myFutureFunc() async {
+    print("I have a function in the future");
+    await Future.delayed(Duration(seconds: 2));
+    return 12;
+  }
+
+  myFutureFunc().then((value)=>print("My function value is: $value"));
+
+  print("Done with main");
+}
